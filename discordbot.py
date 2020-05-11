db089eba0d8f1303a872eaea0192152f541ece53
ponzu07 <fuzitanirenti@yahoo.co.jp>
2020-05-12 00:18:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,9 +16,9 @@ async def on_command_error(ctx, error):
     
 @bot.event
 async def on_voice_state_update(member, before, after): 
-    if member.guild.id == 監視するサーバーid and (before.channel != after.channel):
+    if member.guild.id == 544882228677705738 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel(通知させたいテキストチャンネルid)
+        alert_channel = client.get_channel(709227262251106344)
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
