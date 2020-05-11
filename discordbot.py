52b70d5acca2ea1efdc31dcae3ccc4e355dec138
ponzu07 <fuzitanirenti@yahoo.co.jp>
2020-05-12 00:11:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+from datetime import datetime, timedelta
 
 bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,6 +13,18 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+    
+@client.event
+async def on_voice_state_update(member, before, after): 
+    if member.guild.id == 監視するサーバーid and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(通知させたいテキストチャンネルid)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
 
 @bot.command()
 async def ping(ctx):
